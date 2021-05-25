1fbf597e48bfdb03cfc8f0ebfbfa02b7d1d6e795
andrico <andrico1234@yahoo.co.uk>
2021-05-25 08:26:42 +0100

    get incrtemental builds working

---
+++ lib/src/checkbox.d.ts
@@ -0,0 +1,10 @@
+export declare class Checkbox extends HTMLElement {
+    get disabled(): boolean;
+    set disabled(val: boolean);
+    constructor();
+    click(): void;
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    attributeChangedCallback(name: string, oldVal: string, newVal: string): void;
+}
+//# sourceMappingURL=checkbox.d.ts.map
